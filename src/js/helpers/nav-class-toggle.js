de0454c798784e65835edd4832b3a4b19e2f80a3
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-18 18:45:12 +0100

    removed scss icomoon variables, todo_array

--- src/js/helpers/nav-class-toggle.js
+++
@@ -1,9 +0,0 @@
-export let navClassToggle = (curr, nav) => {
-    for ( let i of nav ) {
-        if ( i.classList.contains('active') ) {
-            i.classList.remove('active');
-        }
-    }
-
-    curr.classList.add('active');
-};
