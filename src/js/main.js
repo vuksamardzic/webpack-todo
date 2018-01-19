b4c712934e3c42e193dff024bed9993997a02f96
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-19 19:16:47 +0100

    app root w/ listeners on elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,9 @@
-import { addTodoTasks, cleanTodoTasks } from './helpers/crud-todo-tasks'
-import { todo } from './models/todo.model'
-import { todo_array } from './models/todo-array.model'
-import { todoTemplate } from './templates/todo.template'
-import { navClassToggle } from './helpers/nav-class-toggle'
-
+import { navClickHandler, formSubmitHandler } from './helpers/event-handler'
+import { $nav, $form } from './helpers/cached-dom'
 import '../scss/main.scss'
 
-const nav = document.querySelectorAll('.todo-nav');
-
-for ( let i of nav ) {
-    i.addEventListener('click', function (ev) {
-        todo.name = 'vuk';
-        todo_array.push(todoTemplate(todo));
-
-        navClassToggle(this, nav);
-
-        addTodoTasks('.todo-task-list', todo_array);
-    });
+for ( let i of $nav ) {
+    i.addEventListener('click', navClickHandler);
 }
+
+$form.addEventListener('submit', formSubmitHandler);
