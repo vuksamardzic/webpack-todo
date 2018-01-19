9cfd3a3bf950f9fe9da5a2f01b1f4321f6c72673
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-19 19:12:12 +0100

    cached dom elements

---
+++ src/js/helpers/cached-dom.js
@@ -0,0 +1,15 @@
+export let $nav = document.querySelectorAll('.todo-nav');
+
+let temp;
+for (let i of $nav ) {
+    
+    if ( i.innerText.toLowerCase() === 'active' ) {
+        temp = i;
+        break;
+    }
+    
+} 
+
+export let $primaryNav = temp;
+export let $form = document.querySelector('form');
+export let $todoTaskList = document.querySelector('.todo-task-list');
