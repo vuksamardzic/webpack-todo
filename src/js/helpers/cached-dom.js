a424f93d911fc9b3a6dc5387e92e6b67f14751dc
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-24 00:02:04 +0100

    optimized code

--- src/js/helpers/cached-dom.js
+++ src/js/helpers/cached-dom.js
@@ -1,16 +1,6 @@
 export let $nav = document.querySelectorAll('.todo-nav');
-
-let temp;
-for (let i of $nav ) {
-    
-    if ( i.innerText.toLowerCase() === 'active' ) {
-        temp = i;
-        break;
-    }
-    
-} 
-
-export let $primaryNav = temp;
 export let $form = document.querySelector('form');
-export let $input = $form[0];
 export let $todoTaskList = document.querySelector('.todo-task-list');
+
+export let $primaryNav = $nav[0];
+export let $input = $form[0];
