63357a971a69b18a1bfe60df963e5f8b6aa1d38b
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-24 00:03:15 +0100

    name change & stash function added

--- src/js/helpers/local-storage.service.js
+++ src/js/helpers/local-storage.service.js
@@ -1,11 +1,15 @@
 import { todoModelArray } from "../models/todo.model";
 import { _render } from "./render";
 
-export let localStorageLoader = () => {
-    const data = localStorage.getItem('todo-app');
+export let localStorageLoader = (key) => {
+    const data = localStorage.getItem(key);
     
     if ( localStorage !== 'undefined' && data ) {
         JSON.parse(data).map(i => todoModelArray.push(i));
         _render();
     }
 };
+
+export let stashLocalStorage = (key, data) => {
+    localStorage.setItem(key, JSON.stringify(data));
+}
